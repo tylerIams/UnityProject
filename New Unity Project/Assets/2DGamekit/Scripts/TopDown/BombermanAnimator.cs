231049afe6692c0c013bd0020b8fef69dc38e03b
Ryan Shee <ryan.shee@gmail.com>
2018-11-04 21:30:02 -0800

    Added top-down character and example scene

---
+++ New Unity Project/Assets/2DGamekit/Scripts/TopDown/BombermanAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombermanAnimator : MonoBehaviour {
+    private Animator animator;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		
+	}
+
+    public void Animate() {
+
+    }
+}
---
+++ New Unity Project/Assets/2DGamekit/Scripts/TopDown/BombermanMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombermanMovement : MonoBehaviour {
+    public float maxSpeed = 10;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, Input.GetAxis("Vertical") * maxSpeed);
+        Animate();
+    }
+
+    public void Animate() {
+        animator.SetBool("moving", (rb.velocity.x != 0 || rb.velocity.y != 0));
+    }
+}
